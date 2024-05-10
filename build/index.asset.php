b6338bac28fc639bfd7f6153e6feba9048da172e
Enamul Hoque Mohon <enamul.hoque.mohon@gmail.com>
2024-05-10 12:08:54 +0600

    Initial Setup.

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8dc35a9a95164dc48416');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '4e286f7c8d12cb4d9e3b');
